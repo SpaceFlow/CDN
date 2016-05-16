fc2bc3d695dc178c57cdb910d24d82c4fa8e0fba
Kirschn <kirschnkiller@googlemail.com>
2016-05-17 00:17:42 +0200

    Update leserver.js

--- leserver.js
+++ leserver.js
@@ -36,6 +36,24 @@ if (cluster.isMaster) {
     });
     var app = express();
     app.use(fileUpload());
+    app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://sharepic.moe');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+    });
     app.get("/upload", function(req, res) {
         res.end(JSON.stringify({
             "error": "no get service"
